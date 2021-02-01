28c196b71cb57ea23e40d03354419f05ce1b67d0
Dona Jose <donajose@expressbase.com>
2021-02-01 08:32:57 +0000

    ws env check

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
+using ExpressBase.Common;
+using ExpressBase.Common.Constants;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 namespace ExpressBase.AuthServer
@@ -7,17 +9,21 @@ namespace ExpressBase.AuthServer
     public class Program
     {
         public static void Main(string[] args)
-        { 
-            Task.Run(() => EbWebsocket.InitialiseWssConnection());
+        {
+            string env = Environment.GetEnvironmentVariable(EnvironmentConstants.ASPNETCORE_ENVIRONMENT);
+            if (env == CoreConstants.PRODUCTION)
+                Task.Run(() => EbWebsocket.InitialiseWssConnection());
+
             CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>().UseKestrel(options => {
+                .UseStartup<Startup>().UseKestrel(options =>
+                {
                     options.Limits.KeepAliveTimeout = TimeSpan.FromMinutes(7);
                     options.Limits.RequestHeadersTimeout = TimeSpan.FromMinutes(5);
                 })
                 .UseUrls(urls: "http://*:41000/");
-    }  
+    }
 }
